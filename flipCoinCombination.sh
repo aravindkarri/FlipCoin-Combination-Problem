6cff07a8834cebb8d9b65cbcddd9e6a0bab3152e
aravindkarri <aravindkarri98495@gmail.com>
2021-06-12 18:05:52 +0530

    Add: code to store singlet combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,10 +2,27 @@
 
 echo "Welcome to FlipCoin Combination Problem"
 
-randomCheck=$((RANDOM%2))
-if [ $randomCheck -eq 1 ]
-then
-	echo "head"
-else
-	echo "tail"
-fi
+read -p "Enter how many times have to flip the coin : " n
+
+declare -A dict
+
+headCount=0
+tailCount=0
+
+for ((i=0;i<n;i++))
+do
+	randomCheck=$((RANDOM%2))
+	if [ $randomCheck -eq 1 ]
+	then
+		((headCount++))
+		dict[$i]="head"
+	else
+		((tailCount++))
+		dict[$i]="tail"
+	fi
+done
+headPercent=$(( ($headCount*100)/$n ))
+tailPercent=$(( ($tailCount*100)/$n ))
+echo "percentage of head: "$headPercent
+echo "percentage of tail: "$tailPercent
+echo ${dict[@]}
